ac81ca42a4c5d0d9c787b5e80bff5b874636f679
t2k-official <106411070+t2k-official@users.noreply.github.com>
2023-11-29 19:07:02 +0000

    Update login.js

--- Admin/login.js
+++ Admin/login.js
@@ -1,12 +1,22 @@
+let usersData;
+
+fetch('users.json')
+  .then(response => response.json())
+  .then(data => {
+    usersData = data.users;
+  })
+  .catch(error => console.log('Error fetching users data:', error));
+
 function checkLogin() {
   var enteredUsername = document.getElementById('username').value;
   var enteredPassword = document.getElementById('password').value;
 
-  // You can replace these with your actual username and password validation logic
-  var storedUsername = 'example_username';
-  var storedPassword = 'example_password';
+  // Loop through the loaded usersData to check credentials
+  let isValidUser = usersData.some(user => {
+    return user.username === enteredUsername && user.password === enteredPassword;
+  });
 
-  if (enteredUsername === storedUsername && enteredPassword === storedPassword) {
+  if (isValidUser) {
     window.location.href = 'welcome.html'; // Redirect to welcome page upon successful login
   } else {
     alert('Invalid username or password. Please try again.');
